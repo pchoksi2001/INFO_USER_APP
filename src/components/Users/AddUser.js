495fd6ffa233a8357481ceef285bdc95f411c3b2
pushtichoksi <pushtichoksi2001@gmail.com>
2023-09-10 10:35:31 +0530

    initial

---
+++ src/components/Users/AddUser.js
@@ -0,0 +1,62 @@
+import React, { useRef, useState } from "react";
+import Card from "../UI/Card";
+import Button from "../UI/Button";
+import classes from './AddUser.module.css';
+import ErrorModal from "./ErrorModal";
+
+const AddUser = (props) => {
+   const nameInputRef = useRef();
+   const ageInputRef = useRef();
+   const[error, setError]= useState();
+
+    const submitHandler =(event) => {   
+        const enteredName = nameInputRef.current.value;
+        const enteredUserAge = ageInputRef.current.value;
+        event.preventDefault();
+        if(enteredName.trim().length === 0 || enteredUserAge.trim().length ===0 ){
+            setError({
+                title:"Invalid Input",
+                message:"Please enter a valid name and age (non-empty values)."
+            })
+            return;
+        }
+        if(+enteredUserAge < 1){
+            setError({
+                title:"Invalid Age",
+                message:"Please enter a valid age (> 0)."
+            })
+            return;
+        }
+        props.onAddUser(enteredName,enteredUserAge);
+        nameInputRef.current.value = '';
+        ageInputRef.current.value = '';    
+    }   
+    
+    const errorHandler = () => {
+        setError(null);
+    }
+    return (
+        <div>
+        {error && <ErrorModal title={error.title} message={error.message} onConfirm={errorHandler}/>}
+        <Card className={classes.input}>
+        <form onSubmit={submitHandler}>
+            <label htmlFor="username">Username</label>
+            <input  
+            id="username" 
+            type="text" 
+            ref={nameInputRef}
+            />
+            <label htmlFor="age" > Age(Years)</label>
+            <input  
+            id="age" 
+            type="number" 
+            ref={ageInputRef}
+            />
+            <Button type="submit"> Add User</Button>
+        </form>
+        </Card>
+        </div>
+    );
+}; 
+
+export default AddUser;
